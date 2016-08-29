aa6763fdf92d37bd8d9d5e8c31745ce8e747c48e
Oscar Triano dotoscat <user@example.com>
2016-08-29 20:07:43 +0200

    Create a button

--- button.js
+++ button.js
@@ -1,8 +1,12 @@
 'use strict';
 
 class Button{
-  constructor(){
-    
+  constructor(x, y, key, callback, text){
+    this.group = game.add.group(game.world, text);
+    this.button = game.make.button(0, 0, key, callback);
+    this.group.addChild(this.button);
+    this.text = game.make.text(0, 0, text, this.group);
+    this.group.position.set(x, y);
   }
 
 }
